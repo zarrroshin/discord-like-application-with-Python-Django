89b115f3f487d6e9bf1f827c43cc8de2627fa210
zahra <zahraroshani973@gmail.com>
2025-10-17 13:04:42 +0330

    implementing delete button with first structure of search

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render,redirect
-from .models import Room
+from .models import Room,Topic
 from .forms import RoomForm
 # Create your views here.
 
 def home(request):
     rooms = Room.objects.all()
-    context = {"rooms":rooms}
+    topics = Topic.objects.all()
+    context = {"rooms":rooms,'topics':topics}
     return render(request,'base/home.html',context)
 
 def room(request,pk):
@@ -37,5 +38,14 @@ def updateRoom(request,pk):
             return redirect("home")
     context ={'form':form}
     return render(request,'base/room_form.html',context)
+
+
+def deleteRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+
+    return render(request,'base/delete.html',{'obj':room})
 
 
