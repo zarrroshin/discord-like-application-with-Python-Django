ec1d3784645243594c4ecf82d30af21b7e4b1277
zahra <zahraroshani973@gmail.com>
2025-10-21 20:06:21 +0330

    implementing commnet and recent activities

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -58,16 +58,25 @@ def home(request):
     rooms = Room.objects.filter(Q(topic__name__contains = q)| Q(name__icontains=q) | Q(description__icontains=q))
     room_count = rooms.count()
     topics = Topic.objects.all()
-    context = {"rooms":rooms,'topics':topics,"room_count":room_count}
+    room_messages = Message.objects.all()
+
+
+
+
+    context = {"rooms":rooms,'topics':topics,"room_count":room_count,'room_messages':room_messages}
     return render(request,'base/home.html',context)
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    
+    
     if request.method == "POST":
         message = Message.objects.create(user=request.user,room=room,body=request.POST.get("body"))
+        room.participants.add(request.user)
         return redirect('room',pk=room.id)
-    context = {"room":room,'room_messages':room_messages}
+    context = {"room":room,'room_messages':room_messages,'participants':participants}
     return render(request,'base/room.html',context)
 
 @login_required(login_url='login')
@@ -107,4 +116,15 @@ def deleteRoom(request,pk):
         return redirect('home')
 
     return render(request,'base/delete.html',{'obj':room})
+
+@login_required(login_url='login')
+def deleteMessage(request,pk):
+    message = Message.objects.get(id=pk)
+    if request.user != message.user:
+        return HttpResponse("you are not allowed here!")
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+
+    return render(request,'base/delete.html',{'obj':message})
 
