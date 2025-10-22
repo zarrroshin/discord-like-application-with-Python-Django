07df4d740a29f274824c854cb2b0d9e8cbc2d1f7
zahra <zahraroshani973@gmail.com>
2025-10-22 20:50:57 +0330

    adding theme

--- studybud/studybud/settings.py
+++ studybud/studybud/settings.py
@@ -118,6 +118,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+# STATIC_ROOT = 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
