08fb96622e3849d51b8d61ff26fc95900101b13e
zahra <zahraroshani973@gmail.com>
2025-11-02 20:54:46 +0330

    implementing pages

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -57,7 +57,7 @@ def home(request):
     q  = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(Q(topic__name__contains = q)| Q(name__icontains=q) | Q(description__icontains=q))
     room_count = rooms.count()
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
 
@@ -159,3 +159,19 @@ def updateUser(request):
         return redirect('user-profile',pk=request.user.id)
 
     return render(request,'base/update-user.html',{"form":form})
+
+
+
+def topicsPage(request):
+    q  = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    topics = Topic.objects.filter(name__icontains=q)
+
+    return render(request,'base/topics.html',{'topics': topics})
+
+
+
+def activitiyPage(request):
+
+    room_messages = Message.objects.all()
+    return render(request,'base/activity.html',{'room_messages' : room_messages})
