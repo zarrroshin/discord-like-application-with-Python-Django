b7dfea2cd76532a817b59cac912685505ded510d
zahra <zahraroshani973@gmail.com>
2025-11-05 20:12:41 +0330

    adding simple api and simple website to check

---
+++ studybud/base/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.getRoute),
+    path('rooms/',views.getRooms),
+
+    path('room/<str:pk>/',views.getRoom),
+]
