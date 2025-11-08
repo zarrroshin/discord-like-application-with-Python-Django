a24bd6d955dbec44392030ee6734cc9d8503f1cb
zahra <zahraroshani973@gmail.com>
2025-11-08 14:49:40 +0330

    changing user model

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,5 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User
+
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    name = models.CharField(max_length=200,null=True)
+    email = models.EmailField(null=True,unique=True)
+    bio = models.TextField(null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+
 
 # Create your models here.
 class Topic(models.Model):
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.http import HttpResponse
-from .models import Room,Topic,Message
+from .models import Room,Topic,Message,User
 from django.db.models import Q
 from .forms import RoomForm,UserForm
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 # Create your views here.
