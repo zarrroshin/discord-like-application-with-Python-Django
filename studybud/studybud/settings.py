5228f7537f085b587b3b86793857a7a671a6eb66
zahra <zahraroshani973@gmail.com>
2025-11-08 17:15:18 +0330

    adding profile feature to users

--- studybud/studybud/settings.py
+++ studybud/studybud/settings.py
@@ -123,10 +123,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-
+MEDIA_ROOT = BASE_DIR / 'static/images'
 # STATIC_ROOT = 
 
 # Default primary key field type
